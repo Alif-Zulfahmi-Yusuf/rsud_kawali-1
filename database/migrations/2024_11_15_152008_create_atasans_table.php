2eeb06a8fe9a9650a2273edf09c64d883a88501d
Alif Zulfahmi Yusuf <123967699+AlifZulfahmi@users.noreply.github.com>
2024-12-07 12:56:51 +0700

    update

--- database/migrations/2024_11_15_152008_create_atasans_table.php
+++ database/migrations/2024_11_15_152008_create_atasans_table.php
@@ -14,7 +14,6 @@ return new class extends Migration
         Schema::create('atasans', function (Blueprint $table) {
             $table->id();
             $table->uuid('uuid');
-            $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
             $table->string('nip');
             $table->string('name');
             $table->string('jabatan');
